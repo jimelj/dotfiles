de2b8a4683c172932a449facbcc28d1dbf2fb945
Jimel Joseph <3507684+jimelj@users.noreply.github.com>
2024-12-18 11:31:13 -0500

    final trackpad updates

--- setup_homebrew.zsh
+++ setup_homebrew.zsh
@@ -7,9 +7,8 @@ if exists brew; then
 else 
   echo "Brew doesn't exist, continuing with install"
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-  echo >> /Users/test/.zprofile
-  echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/test/.zprofile
-  eval "$(/opt/homebrew/bin/brew shellenv)"
+  echo >> /Users/jimelj/.zprofile
+  echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/jimelj/.zprofile
   eval "$(/opt/homebrew/bin/brew shellenv)"
 
 # (echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/jimelj/.zprofile
